144c0cb9b20fbb9cbcf312ee8da510cf75606178
eniiku <enikuauth@gmail.com>
2022-09-04 23:57:51 +0100

    chore: refactor code to accomodate chagnes in file structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
-import { Route, Routes } from 'react-router-dom';
-import Layout from './Layout';
-import { About, Contact, Home } from './pages/index';
+import { Navbar } from './components';
+import { About, Contact, Works } from './container';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="contact" element={<Contact />} />
-      </Route>
-    </Routes>
+    <>
+      <Navbar />
+      <About />
+      <Contact />
+      <Works />
+    </>
   );
 }
 
