103ec6f52ccad106d535c4bba33f2021309d2e9e
eniiku <enikuauth@gmail.com>
2022-09-04 23:33:19 +0100

    chore: re-model existing file structure

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,14 @@
-const Navbar = () => <nav>Navbar</nav>;
+const Navbar = () => (
+  <nav>
+    <p>eniku.</p>
+    <ul>
+      {['about', 'contact'].map((item) => (
+        <li key={item}>
+          <a href={`#${item}`}>{item}</a>
+        </li>
+      ))}
+    </ul>
+  </nav>
+);
 
 export default Navbar;
