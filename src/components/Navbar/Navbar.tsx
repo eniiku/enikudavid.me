54d894c7d1d6a5c4ef497e25e45a33c365ebc83a
eniiku <enikuauth@gmail.com>
2022-09-06 17:31:23 +0100

    feat: add logic for responsiveness in Navbar's mobile menu

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,19 +1,47 @@
+import { useState } from 'react';
+import { HiMenu, HiX } from 'react-icons/hi';
 import './Navbar.scss';
 
-const Navbar = () => (
-  <nav className="app__navbar flex--between">
-    <p className="app__navbar--logo">eniku.</p>
-    <ul className="app__navbar--links flex--center p-text">
-      {['works', 'about'].map((item) => (
-        <li key={item}>
-          <a href={`#${item}`}>{item}</a>
-        </li>
-      ))}
-      <li>
+const Navbar = () => {
+  const [menu, setMenu] = useState(false);
+
+  function toggleMenu() {
+    setMenu((prevMenu) => !prevMenu);
+  }
+
+  return (
+    <nav className="app__navbar flex--between">
+      <p className="app__navbar--logo">eniku.</p>
+      <div className="app__navbar--links flex--center p--text">
+        <ul className="flex--center">
+          {['works', 'about'].map((item) => (
+            <li key={item}>
+              <a href={`#${item}`}>{item}</a>
+            </li>
+          ))}
+        </ul>
         <button className="app__navbar--btn">Contact Me</button>
-      </li>
-    </ul>
-  </nav>
-);
+      </div>
+      <div className="app__navbar--links flex--center p--text">
+        <button className="app__navbar--btn">Contact Me</button>
+        <HiMenu onClick={toggleMenu} />
+        {menu && (
+          <div>
+            <HiX onClick={toggleMenu} />
+            <ul className="flex--center">
+              {['works', 'about'].map((item) => (
+                <li key={`link-${item}`}>
+                  <a href={`#${item}`} onClick={toggleMenu}>
+                    {item}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
