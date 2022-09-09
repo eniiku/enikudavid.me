fc05f45623393ff41aabfc8ca135ddc8c29f91d9
eniiku <enikuauth@gmail.com>
2022-09-09 15:29:29 +0100

    feat: add logic for sending message

--- src/container/Contact/Contact.tsx
+++ src/container/Contact/Contact.tsx
@@ -1,19 +1,37 @@
 import { useState } from 'react';
+import './Contact.scss';
 
 const Contact = () => {
-  const [formData, setformData] = useState({
+  const [formData, setFormData] = useState({
     name: '',
     email: '',
     message: '',
   });
+  const [isLoading, setIsLoading] = useState(false);
 
   const { name, email, message } = formData;
+
+  function handleInput(e: any) {
+    const { name, value } = e.target;
+
+    setFormData({ ...formData, [name]: value });
+  }
+
+  function handleSubmit() {
+    setIsLoading(true);
+
+    console.log('successfull!');
+
+    setIsLoading(false);
+  }
+
+  console.log(formData);
 
   return (
     <section>
       <h1>Get In Touch</h1>
       <div>
-        <div>
+        <div className="p--text">
           <span>
             You have some work for me, an idea you think i can help with, want
             to collaborate with me or just say hello ?
@@ -27,20 +45,25 @@ const Contact = () => {
               value={name}
               name="name"
               placeholder="Your Full Name..."
+              onChange={handleInput}
             />
             <input
               type="email"
               value={email}
               name="email"
               placeholder="Your Email here..."
+              onChange={handleInput}
             />
             <textarea
               name="message"
               value={message}
               placeholder="Type your message here..."
+              onChange={handleInput}
             />
           </div>
-          <button>Send Message</button>
+          <button onClick={handleSubmit}>
+            {isLoading ? 'Sending...' : 'Send Message'}
+          </button>
         </form>
       </div>
     </section>
