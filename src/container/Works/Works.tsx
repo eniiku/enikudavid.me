14605759f32d7fd9b67487de9206c725e04399bf
eniiku <enikuauth@gmail.com>
2022-10-06 21:35:29 +0100

    feat(Works): add basic carousel logic

--- src/container/Works/Works.tsx
+++ src/container/Works/Works.tsx
@@ -1,6 +1,28 @@
+import { useEffect } from 'react';
 import './Works.scss';
 
 const Works = () => {
+  const buttons: any = document.querySelectorAll('[data-carousel-button]');
+
+  useEffect(() => {
+    buttons.forEach((button: any) => {
+      button.addEventListener('click', function () {
+        const offset = button.dataset.carouselButton === 'next' ? 1 : -1;
+        const slides = button
+          .closest('[data-carousel]')
+          .querySelector('[data-slides]');
+
+        const activeSlide = slides.querySelector('[data-active]');
+        let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+        if (newIndex < 0) newIndex + slides.children.length - 1;
+        if (newIndex >= slides.children.length) newIndex = 0;
+
+        slides.children[newIndex].dataset.active = true;
+        delete activeSlide.dataset.active;
+      });
+    });
+  }, ['click']);
+
   return (
     <section id="works" className="app__works app__wrapper">
       <main className="app__works--carousel" data-carousel>
