cb57455c9c18933c6f72f5e84dfcc5f24190f7d3
eniiku <enikuauth@gmail.com>
2022-11-08 22:58:17 +0100

    build(*): clear repo of all files and folders

--- src/container/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as About } from './About/About';
-export { default as Contact } from './Contact/Contact';
-export { default as Footer } from './Footer/Footer';
-export { default as Loader } from './Loader/Loader';
-export { default as Works } from './Works/Works';
