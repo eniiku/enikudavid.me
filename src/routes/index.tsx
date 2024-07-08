4e08bfc9eb2476772e0f179a0744d4becf2c67b6
David Enikuomehin <enikuauth@gmail.com>
2024-07-08 23:02:26 +0100

    fix(routes): fix button css styling issue

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
 import { component$, useStyles$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
+import { type DocumentHead } from "@builder.io/qwik-city";
 
 import { css, cx } from "~/styled-system/css";
 import { flex } from "~/styled-system/patterns";
@@ -27,7 +27,7 @@ export default component$(() => {
         })}>Website still in <br /> production 🚧</h1>
 
         <div class={css({ h: 300, mt: 8, pos: 'relative', '& img': { w: 'auto', h: 'full', display: 'block' } })}>
-          <MemeImage class={css({ mx: 'auto' })} />
+          <MemeImage class={css({ mx: 'auto' })} alt="Funny Meme of Lasisi Elenu ciooking" />
         </div>
 
         <p class={css({
@@ -39,7 +39,7 @@ export default component$(() => {
           textAlign: 'center'
         })}>In the meantime...</p>
 
-        <div class={cx(css({ pos: 'relative', zIndex: 100 }), flex({ justify: 'space-between', align: 'center' }))}>
+        <div class={cx(css({ pos: 'relative', zIndex: 10 }), flex({ justify: 'space-between', align: 'center' }))}>
           <span class={css({ fontFamily: 'gloria', fontSize: 'xl', lineHeight: "140%", color: 'orange', pos: 'absolute', top: -44, left: -48 })}>Connect with <br /> me on socials</span>
           <span class={css({ fontFamily: 'gloria', fontSize: 'xl', lineHeight: "140%", color: 'green', pos: 'absolute', top: -28, right: -52 })}>Check out the <br /> site in progess</span>
 
@@ -47,7 +47,6 @@ export default component$(() => {
           <ArrorRight class={css({ pos: 'absolute', zIndex: '10', top: '-70%', right: -28 })} />
 
           <button class={css({
-            display: "block",
             border: '3px solid',
             borderColor: ["#532011"],
             bg: "#DB7252",
@@ -57,28 +56,36 @@ export default component$(() => {
             fontFamily: 'helvetica_neue',
             fontSize: '1.25rem',
             fontWeight: 'medium',
-            position: 'relative',
-            zIndex: '1',
-
+            pos: 'relative',
+            transition: 'transform 0.2s ease-out 50ms',
             _before: {
+              boxSizing: 'content-box',
+              pos: 'absolute',
               content: '""',
-              position: 'absolute',
-              top: 3,
-              left: 0,
+              top: 1.5,
+              left: '-3px',
+              right: 0,
               w: "full",
               h: "full",
               bg: "rgba(157, 63, 35, 0.7)",
               rounded: '8px',
               border: '3px solid #532011',
-              zIndex: -1
-            }
-
+              zIndex: -1,
+              transition: 'transform 0.2s ease-out, opacity 0.2s ease-out',
+              transform: 'translateY(0)',
+            },
+            _hover: {
+              transform: 'translateY(4px)',
+              _before: {
+                transform: 'translateY(-4px)',
+                opacity: 0,
+              }
+            },
           })}>
             I'm on Linkedin
           </button>
 
           <button class={css({
-
             pos: 'relative',
             border: '3px solid',
             borderColor: "green",
@@ -89,19 +96,30 @@ export default component$(() => {
             fontFamily: 'helvetica_neue',
             fontSize: '1.25rem',
             fontWeight: 'medium',
-            zIndex: 50,
-            _after: {
+            transition: 'transform 0.2s ease-out 50ms',
+            _before: {
+              boxSizing: 'content-box',
               content: '""',
               pos: 'absolute',
               width: 'full',
               height: 'full',
-              left: 0,
-              top: 3,
+              left: '-3px',
+              top: 1.5,
               border: '3px solid',
               borderColor: "green",
-              bg: "#2E8379",
+              bg: "#63A8A0",
               rounded: '8px',
-              zIndex: -10
+              zIndex: -10,
+              transition: 'transform 0.2s ease-out, opacity 0.2s ease-out',
+              transform: 'translateY(0)',
+              opacity: 1,
+            },
+            _hover: {
+              transform: 'translateY(4px)',
+              _before: {
+                transform: 'translateY(-4px)',
+                opacity: 0,
+              }
             },
           })}>
             View Skeleton
