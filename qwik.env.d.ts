ff08b6e5eccc2547246f2d036695a21fc4eedd4e
David Enikuomehin <enikuauth@gmail.com>
2024-07-09 02:59:05 +0100

    chore: add in posthog envs

--- qwik.env.d.ts
+++ qwik.env.d.ts
@@ -2,3 +2,12 @@
 
 // Add global `vite/client` types. For more info, see: https://vitejs.dev/guide/features#client-types
 /// <reference types="vite/client" />
+
+// type definitions for the environment variables
+interface ImportMetaEnv {
+    readonly PUBLIC_POSTHOG_API_KEY: string
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv
+}
